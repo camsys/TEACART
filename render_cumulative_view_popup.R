34f1750b4a489168bf4d2d7a9eea7ff4bdad561b
Gui Vendemiatti <gvendemiatti@camsys.com>
2024-02-16 16:04:00 -0800

    Function render_cumulative_view_popup.R

---
+++ render_cumulative_view_popup.R
@@ -0,0 +1,9 @@
+### Renders table inside cumulative view popup
+render_cumulative_view_popup <- function(my_rv, my_table_no, my_cols){
+  my_rv[["Projects"]] %>% 
+    make_project_table_cumulative(table_no = my_table_no, cols = my_cols) %>%
+    get_horizon_years(my_rv = my_rv) %>%
+    pivot_wider(names_from = year, values_from = value) %>% 
+    select(-any_of(my_cols)) %>%
+    datatable(rownames = F, options = list(dom = "t"))
+}
