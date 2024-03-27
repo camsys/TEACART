c2a025a75833dc0f18d571665b7153937a2ee2a0
adrienne <58457080+xixiant@users.noreply.github.com>
2024-03-27 15:53:17 -0600

    else if and returned the column orders to their original setting

--- functions/reshaping.R
+++ functions/reshaping.R
@@ -31,8 +31,8 @@ reshaping_projects2 <- function(user_data,
 
   if(length(unique(user_data$col)) == 4){ # when there is only one str field
     y_names = c('year')
-    x_names = c('year')}
-  else if (length(unique(user_data$col)) == 5) { # specifically for tbl9, unit is needed for join. 
+    x_names = c('year')
+  }else if (length(unique(user_data$col)) == 5) { # specifically for tbl9, unit is needed for join. 
     y_names = c('var1','year')
     x_names = c(col1,'year')
   }else if(!is.na(col3)){ # when three columns are needed for joining
